7d369beb14d0f197d3f7411a3ff853e08439917c
Masum-Billah <masum.27cse.ru@gmail.com>
2022-03-09 02:21:36 +0600

    Creating an Express Router

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h2>Home Page</h2><br><br><b>Submitted by :</b><br>Name : Masum Billah<br>Student Id : 1910576118");
+    res.send("<h2>Home Page</h2><h3>Assignment 01</h3><p>Assignment topic : Node Modules, Express and REST API</p></p><br><b>Submitted by :</b><br>Name : Masum Billah<br>Student Id : 1910576118");
 });
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
